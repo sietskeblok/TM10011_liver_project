a8349d94b958bbc0577513874b61742f463965e1
Donnadeleur <ddeleur@tudelft.nl>
2026-02-24 14:19:12 +0100

    inladen data 2

--- assignment.py
+++ assignment.py
@@ -8,7 +8,6 @@
 #%% Data loading functions. Uncomment the one you want to use
 from worcliver.load_data import load_data
 
-
 data = load_data()
 print(f'The number of samples: {len(data.index)}')
 print(f'The number of columns: {len(data.columns)}')
