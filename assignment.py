c691b4cb24939407ab2a144eb06f34737d38df3d
PvDijk03 <pvdijk03@hotmail.com>
2026-02-24 14:28:26 +0100

    Checken of ik dingen kan pushen

--- assignment.py
+++ assignment.py
@@ -11,3 +11,4 @@ from worcliver.load_data import load_data
 data = load_data()
 print(f'The number of samples: {len(data.index)}')
 print(f'The number of columns: {len(data.columns)}')
+print("Hopelijk werkt dit")
