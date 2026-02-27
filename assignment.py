f82df7e4237d7c36daf09fcbe13f46e5bc2f81f6
PvDijk03 <pvdijk03@hotmail.com>
2026-02-27 14:40:27 +0100

    In deze versie is het checken vna de NaN's toegevoegd

--- assignment.py
+++ assignment.py
@@ -11,4 +11,7 @@ from worcliver.load_data import load_data
 data = load_data()
 print(f'The number of samples: {len(data.index)}')
 print(f'The number of columns: {len(data.columns)}')
-print("Hopelijk werkt dit")
+print("Hopelijk werkt dit") 
+
+#Checking for missing values
+print(data.isnull().any().any())
