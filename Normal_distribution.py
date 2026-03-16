54499dd2c6c77b6e84c63e68d9aab7c8f04289c6
Donnadeleur <ddeleur@tudelft.nl>
2026-03-16 13:37:50 +0100

    shapiro wilk

--- Normal_distribution.py
+++ Normal_distribution.py
@@ -27,3 +27,30 @@ non_normal_percentage = (len(non_normal_features) / total_features) * 100
 # Print de resultaten
 print(f"Aantal normaal verdeelde features: {len(normal_features)} ({normal_percentage:.2f}%)")
 print(f"Aantal niet normaal verdeelde features: {len(non_normal_features)} ({non_normal_percentage:.2f}%)")
+
+#Shapiro wilk test voor normaal verdeling
+from scipy.stats import shapiro
+
+
+# Lijsten om resultaten op te slaan
+normal_features_sw = []
+non_normal_features_sw = []
+
+# Voer de Shapiro-Wilk test uit voor elke feature
+for column in X_train.columns:
+    stat, p_value = shapiro(X_train[column].dropna())
+
+    # Als p < 0.05 -> niet normaal verdeeld
+    if p_value < 0.05:
+        non_normal_features_sw.append(column)
+    else:
+        normal_features_sw.append(column)
+
+# Bereken percentages
+total_features = len(X_train.columns)
+normal_percentage = (len(normal_features_sw) / total_features) * 100
+non_normal_percentage = (len(non_normal_features_sw) / total_features) * 100
+
+# Print resultaten
+print(f"Aantal normaal verdeelde features shapiro wilk: {len(normal_features_sw)} ({normal_percentage:.2f}%)")
+print(f"Aantal niet normaal verdeelde features shapiro wilk: {len(non_normal_features_sw)} ({non_normal_percentage:.2f}%)")
